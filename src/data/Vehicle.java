aa6aaad167c5de2d425f2c1bfcfa274aa8998f8d
Deborah <deborah.nairn@yahoo.com>
2017-02-15 15:59:28 +0100

    Vehicles added

--- src/data/Vehicle.java
+++ src/data/Vehicle.java
@@ -1,10 +1,45 @@
 package data;
 
 public class Vehicle {
-	// capacity
-	// Max distance
-	// cost per Vehicle
-	// cost per day
-	// cost per distance
-	// id
+	private int capacity;
+	private int maxDistance;
+	private int costPerVehicle;
+	private int costPerDay;
+	private int costPerDistance;
+	private int id;
+	
+	public public Vehicle(int capacity,int maxDistance,int costPerVehicle,int costPerDay,int costPerDistance,int id ) {
+		this.capacity=capacity;
+		this.maxDistance=maxDistance;
+		this.costPerVehicle=costPerVehicle;
+		this.costPerDay=costPerDay;
+		this.costPerDistance=costPerDistance;
+		this.id=id;
+		
+	
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public int getMaxDistance() {
+		return maxDistance;
+	}
+
+	public int getCostPerVehicle() {
+		return costPerVehicle;
+	}
+
+	public int getCostPerDay() {
+		return costPerDay;
+	}
+
+	public int getCostPerDistance() {
+		return costPerDistance;
+	}
+
+	public int getId() {
+		return id;
+	} 
 }
