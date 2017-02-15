7761e4cedb0b1eb448245a3b121ea688cee6d5ec
Deborah <deborah.nairn@yahoo.com>
2017-02-15 16:04:26 +0100

    Location Added

--- src/data/Location.java
+++ src/data/Location.java
@@ -1,6 +1,23 @@
 package data;
 
 public class Location {
+	
+	private int x;
+	private int y;
+	
+	public Location(int x, int y){
+		this.x=x;
+		this.y=y;
+		
+	}
 
-	//Coordinate
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	
 }
