1a5ca5f0207fcf6747babb69f5d8721161500949
Carmen Zarco Lens <cachuti@carmens-mbp-7.wireless.unimaas.local>
2017-02-15 16:14:49 +0100

    initialized the request class

--- src/data/Request.java
+++ src/data/Request.java
@@ -1,10 +1,51 @@
 package data;
 
 public class Request {
-	// id
-	// location id
-	// Time window
-	// tool id
-	// time of usage
-	// # of tools
+	
+	private int id;
+	private Location location;
+	private int startTime;
+	private int endTime;
+	private Tool tool;
+	private int usageTime;
+	private int amountOfTools;
+	
+	public Request(int id, Location location, int startTime, int endTime, Tool tool, int usageTime, int amountOfTools) {
+		this.id = id;
+		this.location = location;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.tool = tool;
+		this.usageTime = usageTime;
+		this.amountOfTools = amountOfTools;
+	}
+	
+	public Location getLocation() {
+		return location;
+	}
+
+	public int getStartTime() {
+		return startTime;
+	}
+
+	public int getEndTime() {
+		return endTime;
+	}
+	
+	public Tool getTool() {
+		return tool;
+	}
+
+	public int getUsageTime() {
+		return usageTime;
+	}
+
+	public int getAmountOfTools() {
+		return amountOfTools;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
 }
