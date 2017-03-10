84aa0fd215f39ea97b0c3d21c2039b454acddd81
Deborah <deborah.nairn@yahoo.com>
2017-03-10 10:34:04 +0100

    Creation of solution file

---
+++ src/solution/Solution.java
@@ -0,0 +1,22 @@
+package solution;
+
+public class Solution {
+	private int maxNumberOfVehicles;
+	private int toolUsed;
+	private int distance;
+	private int cost;
+	
+	public Solution(int maxNumberOfVehicles, int toolUsed, int distance, int cost){
+		this.maxNumberOfVehicles=maxNumberOfVehicles;
+		this.toolUsed=toolUsed;
+		this.distance=distance;
+		this.cost=cost;
+		
+		
+		
+		
+		
+		
+	}
+
+}
