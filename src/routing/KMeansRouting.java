33a013d2726816bb09dcfbeba50d5ef761f65652
Alderia <karsten.seelert@web.de>
2017-06-15 00:22:53 +0200

    Adjusted KMeans so it works multi dimensional

--- src/routing/KMeansRouting.java
+++ src/routing/KMeansRouting.java
@@ -1,6 +1,7 @@
 package routing;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -12,175 +13,137 @@ import java.util.TreeSet;
 import data.DataController;
 import data.VehicleAction;
 import data.VehicleInformation;
+import util.RoutingUtil;
 
-public class KMeansRouting implements Routing{
-	
-	private static final boolean capacityVariance = false;
-	
+public class KMeansRouting implements Routing {
+
+
+	private DataController data = null;
+
 	public KMeansRouting() {
 	}
 
-	
-	public List<VehicleInformation> getRouting(DataController data, List<VehicleAction> dataSet,List<MandatoryConnection> conLoc) {
+	public List<VehicleInformation> getRouting(DataController data, List<VehicleAction> simpleLoc,
+			List<MandatoryConnection> conLoc) {
 		List<VehicleInformation> ret = null;
 		boolean foundSolution = false;
+		this.data = data;
 		int k = 1;
-		while(!foundSolution){
-			Set<MeanCoord> meanSet = initRandomMeans(k, dataSet);
-			Map<MeanCoord,List<VehicleAction>> clusters = null;
-			boolean noChange = true;
-			while(noChange){
-				clusters = assignToMean( meanSet, dataSet);
-				Set<MeanCoord> newMeanSet = calcNewMean(clusters);
-				if(meanSet.containsAll(newMeanSet)){
-					noChange = false;
+		List<RoutingElement> dataSet = new ArrayList<>(simpleLoc);
+		// dataSet.add(conLoc);
+		try {
+			while (!foundSolution) {
+				Set<double[]> meanSet = null;
+				meanSet = initRandomMeans(k, dataSet);
+				Map<double[], List<RoutingElement>> clusters = null;
+				boolean noChange = true;
+				while (noChange) {
+					clusters = assignToMean(meanSet, dataSet);
+					Set<double[]> newMeanSet = calcNewMean(clusters);
+					if (meanSet.containsAll(newMeanSet)) {
+						noChange = false;
+					}
+					meanSet = newMeanSet;
 				}
-				meanSet = newMeanSet;			
-			};
-			
-			//TODO Find a routing solution for List<VehicleAction> in $clusters
-			// use interface to make it easy to swap between routing algorithms where, List<VehicleAction> is solver by one vehicle ?
-			//
-			
-			//TODO delete
-			if(k == 5){
-				foundSolution = true;
+				
+
+				// TODO Find a routing solution for List<VehicleAction> in
+				// $clusters
+				// use interface to make it easy to swap between routing
+				// algorithms where, List<VehicleAction> is solver by one
+				// vehicle ?
+				//
+
+				// TODO delete
+				if (k == 5) {
+					foundSolution = true;
+				}
+
+				// increase k until found solution
+				++k;
 			}
-			
-			
-			//increase k until found solution
-			++k;
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
+
 		return ret;
 	}
 
-	
-	
-	private Map<MeanCoord,List<VehicleAction>> assignToMean(Set<MeanCoord> meanSet, List<VehicleAction> dataSet ){
-		Map<MeanCoord,List<VehicleAction>> ret = new TreeMap<>();
-		for(MeanCoord mean: meanSet){
+	private Map<double[], List<RoutingElement>> assignToMean(Set<double[]> meanSet, List<RoutingElement> dataSet) {
+		Map<double[], List<RoutingElement>> ret = new TreeMap<>();
+		for (double[] mean : meanSet) {
 			ret.put(mean, new ArrayList<>());
 		}
 
-		for(int actionPos = 0 ; actionPos<dataSet.size(); ++actionPos){
-			VehicleAction currAction = dataSet.get(actionPos);
-			MeanCoord minMean = null;
+		for (int actionPos = 0; actionPos < dataSet.size(); ++actionPos) {
+			RoutingElement currElem = dataSet.get(actionPos);
+			double[] minMean = null;
 			double minVariance = Double.MAX_VALUE;
-			for(MeanCoord mean :meanSet){
-				double variance = getVariance(mean, currAction);
-				if(variance < minVariance){
+			for (double[] mean : meanSet) {
+				double variance = getVariance(mean, currElem);
+				if (variance < minVariance) {
 					minMean = mean;
 					minVariance = variance;
 				}
-			}	
-			ret.get(minMean).add(currAction);
-		}
-		return ret;
-	}
-	
-	
-	private Set<MeanCoord> calcNewMean(Map<MeanCoord,List<VehicleAction>> clusters){
-		Set<MeanCoord> ret = new TreeSet<>();
-		for(MeanCoord mean : clusters.keySet()){
-			List<VehicleAction> dataList = clusters.get(mean);
-			int clusterSize = dataList.size();
-			
-			//TODO Handle overflow
-			double meanX = 0,meanY = 0, meanCapacity = 0;	
-			double safeX = 0, safeY= 0, safeCapacity = 0;
-			for(int i = 0; i < dataList.size(); ++i){
-				VehicleAction action = dataList.get(i);
-				double actionX = action.getRequest().getLocation().getX();
-				double actionY = action.getRequest().getLocation().getY();
-				int partAmount = action.getRequest().getAmountOfTools();
-				int partSize = action.getRequest().getTool().getSize();
-				double actionCapacity = partAmount*partSize;
-
-				//check for overflow (might want to do underflow but we are only working with positive numbers (variance was squared))
-				boolean isOverflowSafe = (safeX >0 && actionX > Double.MAX_VALUE- safeX );
-				isOverflowSafe = isOverflowSafe && (safeY >0 && actionY > Double.MAX_VALUE- safeY);
-				isOverflowSafe = isOverflowSafe && (safeCapacity >0 && actionCapacity > Double.MAX_VALUE- safeCapacity);			
-				if (isOverflowSafe){
-					safeX+=actionX;
-					safeY+=actionY;
-					safeCapacity=actionCapacity;
-				}else {
-					meanX += safeX/clusterSize;
-					meanY += safeY/clusterSize;
-					meanCapacity += safeCapacity/clusterSize;
-					
-					safeX=0;
-					safeX=0;
-					safeCapacity=0;
-				}
-				
-				/*meanX += action.getRequest().getLocation().getX();
-				meanY += action.getRequest().getLocation().getY();
-				int partAmount = action.getRequest().getAmountOfTools();
-				int partSize = action.getRequest().getTool().getSize();
-				meanCapacity = partAmount*partSize;		*/	
 			}
-			meanX += safeX/clusterSize;
-			meanY += safeY/clusterSize;
-			meanCapacity += safeCapacity /clusterSize;
-			
-			
-			ret.add(new MeanCoord(meanX, meanY, meanCapacity));
+			ret.get(minMean).add(currElem);
 		}
-		
-		return ret;		
-	}
-	
-	private double getVariance( MeanCoord mean, VehicleAction action){
-		double ret = 0.0;
-		ret+= Math.pow(action.getRequest().getLocation().getX()-mean.x , 2);
-		ret+= Math.pow(action.getRequest().getLocation().getY()-mean.y , 2);
-		
-		if(capacityVariance){
-			int partAmount = action.getRequest().getAmountOfTools();
-			int partSize = action.getRequest().getTool().getSize();		
-			ret+= Math.pow((partAmount*partSize)-mean.capacity , 2);			
-		}	
 		return ret;
 	}
+
 	
 	/**
-	 * Get k randomly selected means without duplicates, where capacity is zero (to make sure that a location is not twice in the set)
+	 * 
+	 * @param clusters
+	 * @return Center of Mass of cluster values (RoutingElemen)
+	 */
+	private Set<double[]> calcNewMean(Map<double[], List<RoutingElement>> clusters) {
+		Set<double[]> ret = new TreeSet<>();
+		for (double[] mean : clusters.keySet()) {
+			double[] newMean = RoutingUtil.getCenterOfMass(data, clusters.get(mean));
+			ret.add(newMean);
+		}
+		return ret;
+	}
+
+	private double getVariance(double[] mean, RoutingElement routeElem) {
+		double ret = 0.0;
+		double[] spaceVec = routeElem.getSpaceVector(data);
+		assert mean.length == spaceVec.length;
+		for (int i = 0; i < mean.length; ++i) {
+			ret += Math.pow(spaceVec[i] - mean[i], 2);
+		}
+		return ret;
+	}
+
+	/**
+	 * Get k randomly selected means without duplicates, where capacity is zero
+	 * (to make sure that a location is not twice in the set)
 	 * 
 	 * @param k
 	 * @param dataSet
 	 * @return
+	 * @throws Exception
 	 */
-	private Set<MeanCoord> initRandomMeans(int k ,List<VehicleAction> dataSet){
-		Set<MeanCoord> ret = new TreeSet<>();
+	private Set<double[]> initRandomMeans(int k, List<RoutingElement> dataSet) throws Exception {
+		Set<double[]> ret = new TreeSet<>();
+		Set<Integer> rngList = new HashSet<>();
+		rngList.add(-1);
 		Random rng = new Random();
-		while(ret.size() < k){
-			int listPos = rng.nextInt(dataSet.size());
-			MeanCoord tmp = convertActionToCoord(dataSet.get(listPos));
-			tmp.capacity = 0;
-			ret.add(tmp);			
+		if (k > dataSet.size()) {
+			throw new Exception("KMeanRouting: k is too large");
+		}
+		while (ret.size() < k) {
+			// make sure no double randoms
+			int listPos = -1;
+			while (rngList.contains(listPos)) {
+				listPos = rng.nextInt(dataSet.size());
+			}
+			rngList.add(listPos);
+
+			double[] tmp = dataSet.get(listPos).getSpaceVector(data);
+			ret.add(tmp);
 		}
 		return ret;
-	}
-
-	private MeanCoord convertActionToCoord(VehicleAction action	){
-		double x = action.getRequest().getLocation().getX();
-		double y = action.getRequest().getLocation().getY();
-		int partAmount = action.getRequest().getAmountOfTools();
-		int partSize = action.getRequest().getTool().getSize();
-		double capacity = partAmount*partSize;		
-		return new MeanCoord(x, y, capacity);
-	}
-	
-}
-
-class MeanCoord{
-	
-	public double x,y,capacity;
-	public MeanCoord(double x, double y, double capacity) {
-		this.x = x;
-		this.y = y;
-		this.capacity = capacity;
 	}
 }
--- src/routing/MandatoryConnection.java
+++ src/routing/MandatoryConnection.java
@@ -1,24 +1,34 @@
 package routing;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import data.DataController;
+import data.Location;
 import data.Request;
 import data.VehicleAction;
 import data.VehicleAction.Action;
+import util.DataUtil;
+import util.RoutingUtil;
 
-public class MandatoryConnection {
+public class MandatoryConnection extends RoutingElement {
 	private List<VehicleAction> pickupList;
 	private List<VehicleAction> deliverList;
+	
+	//if any changes happened since last call{spaceVector}
+	boolean change[] = new boolean[1];
 	
 	public MandatoryConnection() {
 		pickupList = new LinkedList<>();
 		deliverList = new LinkedList<>();
+		setChangeTrue();
 	}
 	
 	public MandatoryConnection(List<VehicleAction> pickUpList, List<VehicleAction> deliverList ){
 		this.pickupList = new LinkedList<>(pickupList);
 		this.deliverList = new LinkedList<>(deliverList);
+		setChangeTrue();
 	}
 	
 	public MandatoryConnection(MandatoryConnection manCon){
@@ -28,18 +38,21 @@ public class MandatoryConnection {
 			this.pickupList.addAll(manCon.getPickupList());
 			this.deliverList.addAll(manCon.getDeliverList());			
 		}
+		setChangeTrue();
 	}
 	
 	
 	public void addPickupList(VehicleAction pickup){
 		if(!pickupList.contains(pickup)){
-			pickupList.add(pickup);			
+			pickupList.add(pickup);	
+			setChangeTrue();
 		}
 	}
 	
 	public void addDeliverList(VehicleAction deliver){
 		if(!deliverList.contains(deliver)){
-			deliverList.add(deliver);			
+			deliverList.add(deliver);
+			setChangeTrue();
 		}
 	}
 	
@@ -74,14 +87,39 @@ public class MandatoryConnection {
 		return ret;
 	}
 	
-	
-
 	public final List<VehicleAction> getPickupList() {
 		return pickupList;
 	}
-
+	
 	public final List<VehicleAction> getDeliverList() {
 		return deliverList;
+	}
+	private void setChangeTrue(){
+		for(int i=0;i<change.length;++i){
+			change[i]= true;
+		}
+	}
+	
+
+
+	@Override
+	public double[] getSpaceVector(DataController data) {
+		if(change[0]){
+			spaceVector = null;
+		}
+		if(spaceVector == null){
+			List<RoutingElement> rtElemList = new ArrayList<>(getRoute());
+			spaceVector = RoutingUtil.getCenterOfMass(data, rtElemList);
+			if(spaceVector == null){
+				spaceVector = new double[data.getToolList().size()+LOCATION_DIMENSION];
+			}
+			change[0] = false;
+		}	
+		return spaceVector;
+	}
+
+	public List<VehicleAction> getRouteElement() {
+		return getRoute();
 	}
 
 }
